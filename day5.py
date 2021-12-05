3ed02f9347039ee1314ba53b29436883b72b0f76
Brynjar Bragason <brynjarb@ccpgames.com>
2021-12-05 12:23:38 +0000

    Complete day 5

--- day5.py
+++ day5.py
@@ -4,19 +4,22 @@ pp = pprint.PrettyPrinter(indent=4)
 # Read the input
 f = open("input5.txt", "r")
 lines = f.read().splitlines()
-#pp.pprint(lines)
 
 
 class Hydroline:
     def __init__(self, lines):
+        # Flag to check the diagonl lines or not, for part 1 and 2
+        checkDiagonalLines = False
         grid = []
-        for x in range(10):
+        gridSize = 1000
+        # Create grid with inital value 0
+        for x in range(gridSize):
             nextLine = []
-            for y in range(10):
+            for y in range(gridSize):
                 nextLine.append(0)
             grid.append(nextLine)
-        lines = ['0,1 -> 3,4']
         for line in lines:
+            # read the lines and get the values
             line = line.split(' -> ')
             first= line[0].split(',')
             last = line[1].split(',')
@@ -24,52 +27,40 @@ class Hydroline:
             y1 = int(first[1])
             x2 = int(last[0])
             y2 = int(last[1])
-            print(x1, y1, x2, y2)
-            if (x1 == y2 and x2 == y2) or (x1 == y1 and x2 == y2):
-                if x1 - x2 == y1 - y2:
-                    if x1 - x2 < 0:
-                        i = x1
-                        j = y1
-                        while i <= x2:
-                            grid[j][i] += 1
-                            i += 1
-                            j += 1
-                    elif x1-x2 > 0:
-                        i = x1
-                        j = y1
-                        while i <= x2:
-                            grid[j][i] += 1
-                            i -= 1
-                            j -= 1
-
-                '''
-                if x1 < x2:
-                    if y1 < y2:
-                        print('here2')
-                        i = x1
-                        j = y1
-                        while i <= x2:
-                            grid[j][i] += 1
-                            i += 1
-                            j += 1
-                    if y1 > y2:
-                        print('here1')
-                        i = x1
-                        j = y1
-                        while i <= x2:
-                            grid[j][i] += 1
-                            i += 1
-                            j -= 1
-                elif x1 > x2:
-                    if y1 < y2:
-                        i = x1
-                        j = y1
-                        while i >= x2:
-                            grid[j][i] += 1
-                            i -= 1
-                            j -= 1
-                    if y1 > y2:
-                        pass
+            # Check diagonal lines
+            if x1 - x2 == y1 - y2 and checkDiagonalLines:
+                if x1 - x2 < 0:
+                    i = x1
+                    j = y1
+                    while i <= x2:
+                        grid[j][i] += 1
+                        i += 1
+                        j += 1
+                elif x1 - x2 > 0:
+                    i = x1
+                    j = y1
+                    while i >= x2:
+                        grid[j][i] += 1
+                        i -= 1
+                        j -= 1
+            # Check other diagonal lines
+            if -(x1 - x2) == y1 - y2 and checkDiagonalLines:
+                i = x1
+                j = y1
+                # first we do the one way if it is posible
+                while i <= x2:
+                    grid[j][i] += 1
+                    i += 1
+                    j -= 1
+                
+                # Then we do the other way if that is posible
+                i = x1
+                while i >= x2:
+                    grid[j][i] += 1
+                    i -= 1
+                    j += 1  
+            # Check straight lines  
+            
             elif x1 == x2:
                 i = min(y1, y2)
                 end = max(y1, y2)
@@ -82,8 +73,7 @@ class Hydroline:
                 while i <= end:
                     grid[y1][i] += 1
                     i += 1
-            '''
-        pp.pprint(grid)
+        #Count where it is 2 or higher
         counter = 0
         for line in grid:
             for value in line:
